d8185825867eebe7c3f0b6d4c151d70f58de892b
msand2808 <misael.sandoval2808@gmail.com>
2026-06-09 17:36:20 -0600

    agregado el ejemplo de duplicar el valor de una variable

---
+++ 6 - duplicar.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/* 4. Escribe void duplicar(int *n) que duplique el valor recibido. */
+
+void duplicar(int *n);
+
+int main(){
+
+    int numero = 5;
+
+    printf("Valor actual: %d\n", numero);
+
+    duplicar(&numero);
+
+    printf("Valor duplicado: %d\n", numero);
+
+}
+
+void duplicar(int *n){
+    *n = 2* (*n);
+
+}
